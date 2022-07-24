29b888e68c0063d69ae25a917608ffe25ceb6d78
UsiFX <xprjkts@gmail.com>
2022-07-24 18:25:58 +0200

    liquid-builder: Clone dependencies
    * This will clone dependencies if `TARGET_CLONE_DEPENDENCIES` is defined
    * This contains GCC, Clang & AnyKernel3 if they are defined
    
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- liquid-builder.sh
+++ liquid-builder.sh
@@ -61,3 +61,74 @@ if [ ${TARGET_USE_TELEGRAM} == 'true' ];then
 	}
 	fi
 fi
+
+if [ $TARGET_CLONE_DEPENDENCIES == 'true' ]; then
+	if [ $TARGET_USE_TELEGRAM == 'true' ]; then
+		tg_send_msg "*Cloning Dependencies!*"
+	fi
+	if [ $TARGET_COMPILER == 'clang' ]; then
+		if ! [ $TARGET_COMPILER_REPOSITORY ]; then
+			echo -e "${red}[x] TARGET_COMPILER_REPOSITORY is undeclared"
+			echo -e "${red}[x] unable to sync."
+			return 0
+		else
+			if [ ! -d ${TARGET_WORKSPACE_DIRECTORY}/clang ]; then
+				echo -e "${blue}[i] Cloning ${TARGET_COMPILER_REPOSITORY}..."
+				git clone --depth=1 "${TARGET_COMPILER_REPOSITORY}" ${TARGET_WORKSPACE_DIRECTORY}/clang &>/dev/null
+				echo -e "${green}[*] Done."
+			else
+				KBUILD_COMPILER_STRING=$("${TARGET_COMPILER_REPOSITORY}"/clang/bin/clang -v 2>&1 | head -n 1 | sed 's/(https..*//' | sed 's/ version//')
+				export KBUILD_COMPILER_STRING
+				export PATH=$TARGET_COMPILER_REPOSITORY/clang/bin/:/usr/bin/:${PATH}
+				echo -e "${blue}[i] Compiler Strings have been set successfully."
+			fi
+		fi
+	elif [ $TARGET_COMPILER == 'gcc' ]; then
+		if ! [ $TARGET_COMPILER_REPOSITORY ]; then
+			echo -e "${red}[x] TARGET_COMPILER_REPOSITORY is undeclared"
+			echo -e "${red}[x] unable to sync."
+			return 0
+		else
+			if [ ! -d ${TARGET_WORKSPACE_DIRECTORY}/gcc64 ]; then
+				echo -e "${blue}[i] Cloning ${TARGET_GCC_COMPILER_REPOSITORY}..."
+				git clone --depth=1 "${TARGET_GCC_COMPILER_REPOSITORY}" ${TARGET_WORKPSACE_DIRECTORY}/gcc64 &>/dev/null
+				echo -e "${green}[*] Done."
+			elif [ ! -d ${TARGET_WORKSPACE_DIRECTORY}/gcc32 ]; then
+				echo -e "${blue}[i] Cloning ${TARGET_GCC_COMPILER32_REPOSITORY}..."
+				git clone --depth=1 "${TARGET_GCC_COMPILER32_REPOSITORY}" ${TARGET_WORKSPACE_DIRECTORY}/gcc32 &>/dev/null
+				echo -e "${green}[*] Done."
+			else
+				KBUILD_COMPILER_STRING=$("${TARGET_WORKSPACE_DIRECTORY}"/gcc64/bin/aarch64-elf-gcc --version | head -n 1)
+				export KBUILD_COMPILER_STRING
+				export PATH="${TARGET_WORKSPACE_DIRECTORY}"/gcc32/bin:"${TARGET_WORKSPACE_DIRECTORY}"/gcc64/bin:/usr/bin/:${PATH}
+				echo -e "${blue}[i] Compiler Strings have been set successfully."
+			fi
+		fi
+	if [ $TARGET_PACKAGE_ANYKERNEL == 'true' ]; then
+		if [ ! -d $TARGET_WORKSPACE_DIRECTORY/anykernel ]; then
+			echo -e "${blue}[i] Cloning ${TARGET_ANYKERNEL_SOURCE}..."
+			git clone --depth=1 "${TARGET_ANYKERNEL_SOURCE}" ${TARGET_WORKSPACE_DIRECTORY}/anykernel &>/dev/null
+			echo -e "${green}[*] Done."
+		fi
+		package ()
+		{
+			echo -e "${blue}[i] Building ZIP..."
+			if [ $TARGET_USE_TELEGRAM == 'true' ]; then
+				tg_send_msg "*Building ZIP!*"
+			fi
+			cd $TARGET_WORKSPACE_DIRECTORY/anykernel
+			cp $TARGET_WORKSPACE_DIRECTORY/out/arch/arm64/boot/Image.gz-dtb $(pwd)
+			zip -r9 "$TARGET_PACKAGE_NAME".zip . -x ".git*" -x "README.md" -x "LICENSE" -x "*.zip" &>/dev/null
+			echo -e "${green}[*] ZIP Built!"
+			if [ $TARGET_USE_TELEGRAM == 'true' ]; then
+				tg_send_msg "*ZIP Built!*"
+				tg_send_file "$TARGET_PACKAGE_NAME.zip" "
+*#$TARGET_SPECIAL_BUILDID
+*compiler:* \`$KBUILD_COMPILER_STRING\`
+*builder:* \`$TARGET_BUILD_USER\`
+*host:* \`$TARGET_BUILD_HOST\`
+*kversion:* \`make kernelversion 2>/dev/null\`
+"
+			fi
+		}
+fi
